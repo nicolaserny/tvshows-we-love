d31a8065d4dd1f99e45b9ffbf747e02ec28e8aca
nicolaserny <nicolas.erny@xcomponent.com>
2020-08-03 15:13:03 +0200

    Use self hosted google fonts

--- src/utils/global.js
+++ src/utils/global.js
@@ -6,6 +6,38 @@ import { device } from "./device";
 
 export const GlobalStyle = createGlobalStyle`
     ${normalize()}
+    @font-face {
+        font-family: "Montserrat";
+        font-style: normal;
+        font-weight: 400;
+        font-display: fallback;
+        src: url("/fonts/montserrat-v14-latin-regular.woff2") format("woff2"),
+          url("/fonts/montserrat-v14-latin-regular.woff") format("woff");
+    }
+    @font-face {
+    font-family: "Montserrat";
+    font-style: normal;
+    font-weight: 600;
+    font-display: fallback;
+    src: url("/fonts/montserrat-v14-latin-600.woff2") format("woff2"),
+        url("/fonts/montserrat-v14-latin-600.woff") format("woff");
+    }
+    @font-face {
+    font-family: "Montserrat";
+    font-style: normal;
+    font-weight: 700;
+    font-display: fallback;
+    src: url("/fonts/montserrat-v14-latin-700.woff2") format("woff2"),
+        url("/fonts/montserrat-v14-latin-700.woff") format("woff");
+    }
+    @font-face {
+    font-family: 'Lora';
+    font-style: normal;
+    font-weight: 700;
+    font-display: fallback;
+    src: url('/fonts/lora-v16-latin-700.woff2') format('woff2'),
+        url('/fonts/lora-v16-latin-700.woff') format('woff');
+    }
     html {
         font-size: 16px;
         box-sizing: border-box;
